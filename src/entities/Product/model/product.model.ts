9fb3ed9dbe1ea0651144266d181d4a54c11f2d53
dmakger <dmakger@yandex.ru>
2024-07-27 19:36:30 +0300

    Add all models

--- src/entities/Product/model/product.model.ts
+++ src/entities/Product/model/product.model.ts
@@ -1,7 +1,40 @@
+import { ICategory } from "@/entities/Metric/model/category.metric.model"
+import { ICharacteristic } from "@/entities/Metric/model/characteristic.metric.model"
+import { IExecutionTime } from "@/entities/Metric/model/executionTime.metric.model"
+
 /**
  * Модель `Продукта`
  */
 export interface IProduct {
     id: number
     title: string
+    price: 160.0,
+    image: string | null,
+    parentId: number,
+    category: ICategory
+    executionTime: IExecutionTime
+}
+
+
+export interface IProductFormCharacteristics {
+    id: number
+    characteristics: ICharacteristic[]
+    executionTime: IExecutionTime
+}
+
+
+
+export interface IProductForm {
+    id: number
+    title: string
+    combinations: IProductFormCharacteristics[]
+}
+
+
+export interface IProductCharacteristicCombination {
+    id: number
+    price: number
+    characteristics: ICharacteristic[]
+    forms: IProductForm[]
+    execution_time: number
 }
