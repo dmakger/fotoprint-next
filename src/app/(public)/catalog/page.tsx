d38e16a3e9c0a8ffe5282158a9441e6b4a6bbf3f
dmakger <dmakger@yandex.ru>
2024-07-27 20:37:00 +0300

    Add starter pack

---
+++ src/app/(public)/catalog/page.tsx
@@ -0,0 +1,9 @@
+import Wrapper1280 from "@/shared/ui/Wrapper/1280/Wrapper1280";
+
+export default function CatalogPage() {
+    return (
+        <Wrapper1280>
+            {/* <ProductListWCategory /> */}
+        </Wrapper1280>
+    )
+}
