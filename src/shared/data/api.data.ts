7c5427f85dbdc8bad65c79ac176218dfc6a04c47
dmakger <dmakger@yandex.ru>
2025-02-08 21:00:10 +0300

    To localhost

--- src/shared/data/api.data.ts
+++ src/shared/data/api.data.ts
@@ -1,10 +1,10 @@
 // ===={ LOGIC IS PRODUCTION }====
-export const IS_PRODUCTION = true
+export const IS_PRODUCTION = false
 
 
 // API
 export const LOCAL_API_URL = "http://127.0.0.1:8000"
-export const PRODUCTION_API_URL = "https://api.fotoprintart.ru/"
+export const PRODUCTION_API_URL = "https://api.fotoprintart.ru"
 
 export const CURRENT_API_URL = IS_PRODUCTION ? PRODUCTION_API_URL : LOCAL_API_URL
 
